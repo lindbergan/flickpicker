524c803c1855588dec96d0518a1d6b9e8c12afd4
Adrian Lindberg <adrlin@student.chalmers.se>
2016-04-25 15:04:34 +0200

    Added methods from UML in FriendDAO

--- app/src/main/java/com/typeof/flickpicker/database/FriendDAO.java
+++ app/src/main/java/com/typeof/flickpicker/database/FriendDAO.java
@@ -1,9 +1,18 @@
 package com.typeof.flickpicker.database;
 
+import com.typeof.flickpicker.core.User;
+
+import java.util.List;
+
 /**
  * FlickPicker
  * Group 22
  * Created on 16-04-25.
  */
 public interface FriendDAO {
+
+    public boolean addFriend(long userId1, long userId2);
+    public List<User> getFriendsFromUserId(long id);
+    public boolean removeFriend();
+
 }
