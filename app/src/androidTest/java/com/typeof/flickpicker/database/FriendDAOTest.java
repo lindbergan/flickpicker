364ea89e98ffaef6f1b2807f7079a1fdd8d80181
Adrian Lindberg <adrlin@student.chalmers.se>
2016-04-25 13:52:43 +0200

    Created FriendDAOTest

---
+++ app/src/androidTest/java/com/typeof/flickpicker/database/FriendDAOTest.java
@@ -0,0 +1,13 @@
+package com.typeof.flickpicker.database;
+
+import android.test.AndroidTestCase;
+
+/**
+ * FlickPicker
+ * Group 22
+ * Created on 16-04-25.
+ */
+
+public class FriendDAOTest extends AndroidTestCase {
+
+}
