c77a7c306f5cbc9dc6a2287e132a4dd587cba82b
Adrian Lindberg <adrlin@student.chalmers.se>
2016-04-25 13:55:28 +0200

    Created setUp() in FriendDAOTest

--- app/src/androidTest/java/com/typeof/flickpicker/database/FriendDAOTest.java
+++ app/src/androidTest/java/com/typeof/flickpicker/database/FriendDAOTest.java
@@ -2,6 +2,8 @@ package com.typeof.flickpicker.database;
 
 import android.test.AndroidTestCase;
 
+import com.typeof.flickpicker.database.sql.SQLFriendDAO;
+
 /**
  * FlickPicker
  * Group 22
@@ -10,4 +12,14 @@ import android.test.AndroidTestCase;
 
 public class FriendDAOTest extends AndroidTestCase {
 
+    private FriendDAO fFriendDAO;
+    private DatabaseSeed fDatabaseSeed;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        fFriendDAO = new SQLFriendDAO(getContext());
+        fDatabaseSeed = new DatabaseSeed(getContext());
+        fDatabaseSeed.seedDatabase();
+    }
 }
