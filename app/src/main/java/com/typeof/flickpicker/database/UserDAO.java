8e8a546c3368c57f8581cd3913323b9f2912785f
Jonathan Sundkvist <jonathansundkvist@hotmail.com>
2016-04-25 15:19:48 +0200

    Defined UserDAO interface

--- app/src/main/java/com/typeof/flickpicker/database/UserDAO.java
+++ app/src/main/java/com/typeof/flickpicker/database/UserDAO.java
@@ -1,9 +1,16 @@
 package com.typeof.flickpicker.database;
 
+import com.typeof.flickpicker.core.User;
+
 /**
  * FlickPicker
  * Group 22
  * Created on 16-04-25.
  */
 public interface UserDAO {
+
+    public User getUserById(long userId);
+    public long saveUser(User user);
+    public int deleteUser(User user);
+
 }
