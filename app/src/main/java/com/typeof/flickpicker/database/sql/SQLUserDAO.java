5d2d31976948f35f059a99d2cbe4faa4d8f5d26f
Jonathan Sundkvist <jonathansundkvist@hotmail.com>
2016-04-26 14:30:42 +0200

    deleteUser written

--- app/src/main/java/com/typeof/flickpicker/database/sql/SQLUserDAO.java
+++ app/src/main/java/com/typeof/flickpicker/database/sql/SQLUserDAO.java
@@ -17,25 +17,12 @@ import com.typeof.flickpicker.database.UserDAO;
  */
 public class SQLUserDAO extends SQLDAO implements UserDAO {
 
-    private SQLiteDatabase db;
-
     public SQLUserDAO(Context ctx) {
         super(ctx);
         //SQLiteDatabaseHelper mDbHelper = new SQLiteDatabaseHelper(ctx);
         //db = mDbHelper.getWritableDatabase();
     }
 
-//    public User find(long id) {
-//        Cursor c = db.rawQuery("SELECT * FROM users WHERE id = ? LIMIT 1", new String[]{String.valueOf(id)});
-//        if (c.getCount() < 1) {
-//            return null;
-//        }
-//
-//        User user = createUserFromDatabaseData(c);
-//        c.close();
-//
-//        return user;
-//    }
 
     public User createUserFromCursor(Cursor c){
         c.moveToFirst();
@@ -69,7 +56,7 @@ public class SQLUserDAO extends SQLDAO implements UserDAO {
 
     @Override
     public int deleteUser(User user) {
-        return this.delete()
+        return super.delete(user, UserTable.UserEntry.TABLE_NAME);
     }
 
 /*
