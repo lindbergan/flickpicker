c6127f1ae2069ecefe67cfeac8403b557658ed89
Adrian Lindberg <adrlin@student.chalmers.se>
2016-04-27 20:03:04 +0200

    Added extra db.exeq lines for FriendDAO and UserDAO

--- app/src/main/java/com/typeof/flickpicker/database/sql/SQLiteDatabaseHelper.java
+++ app/src/main/java/com/typeof/flickpicker/database/sql/SQLiteDatabaseHelper.java
@@ -1,8 +1,12 @@
 package com.typeof.flickpicker.database.sql;
 
 import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteAbortException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.typeof.flickpicker.core.Friend;
 
 public class SQLiteDatabaseHelper extends SQLiteOpenHelper {
 
@@ -17,12 +21,17 @@ public class SQLiteDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(android.database.sqlite.SQLiteDatabase db) {
         db.execSQL(MovieTable.MovieEntry.getSQLCreateTableQuery());
         db.execSQL(RatingTable.RatingEntry.getSQLCreateTableQuery());
+        db.execSQL(FriendTable.FriendEntry.getSQLCreateTableQuery());
+        db.execSQL(UserTable.UserEntry.getSQLCreateTableQuery());
     }
 
     @Override
     public void onUpgrade(android.database.sqlite.SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(MovieTable.MovieEntry.getSQLDropTableQuery());
         db.execSQL(RatingTable.RatingEntry.getSQLDropTableQuery());
+        db.execSQL(FriendTable.FriendEntry.getSQLDropTableQuery());
+        db.execSQL(UserTable.UserEntry.getSQLDropTableQuery());
+
         onCreate(db);
     }
 
