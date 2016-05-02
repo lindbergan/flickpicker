11ea51ef9ed9dc370a08b303c3a1b57479f113b7
Jolo87 <nylundj@student.chalmers.se>
2016-05-02 22:18:23 +0200

    code cleanup in RatingDAO && RatingDAOtest

--- app/src/main/java/com/typeof/flickpicker/database/sql/SQLRatingDAO.java
+++ app/src/main/java/com/typeof/flickpicker/database/sql/SQLRatingDAO.java
@@ -63,7 +63,6 @@ public class SQLRatingDAO extends SQLDAO implements RatingDAO {
         double newCommunityRating = calculateCommunityRating(movie,rating);
         movie.setCommunityRating(newCommunityRating);
         int oldVotes = movie.getNumberOfVotes();
-        double getCommunityRating = movie.getCommunityRating();
         movie.setNumberOfVotes(oldVotes+1);
         sqlMovieDAO.saveMovie(movie);
 
@@ -139,64 +138,4 @@ public class SQLRatingDAO extends SQLDAO implements RatingDAO {
         }
         return sortedMovies;
     }
-
-    /*
-    public Rating find(long id) {
-        try {
-            Cursor c = super.find(id, "ratings");
-            Rating rating = createRatingCursor(c);
-            c.close();
-            return rating;
-        } catch (DatabaseRecordNotFoundException e) {
-            throw new DatabaseRecordNotFoundException(e.getMessage());
-        }
-    }
-
-    public Rating createRatingCursor(Cursor c) {
-        c.moveToFirst();
-        long id = c.getLong(c.getColumnIndex(MovieTable.MovieEntry.COLUMN_NAME_ID));
-        Double rating = c.getDouble(c.getColumnIndex(RatingTable.RatingEntry.COLUMN_NAME_RATING));
-        int movieId = c.getInt(c.getColumnIndex(RatingTable.RatingEntry.COLUMN_NAME_MOVIEID));
-        int userId = c.getInt(c.getColumnIndex(RatingTable.RatingEntry.COLUMN_NAME_USERID));
-        Rating r = new Rating(rating, movieId,userId);
-        r.setId(id);
-        return r;
-    }
-
-    public long save(Rating rating) {
-        ContentValues values = new ContentValues();
-        values.put(RatingTable.RatingEntry.COLUMN_NAME_RATING, rating.getRating());
-        values.put(RatingTable.RatingEntry.COLUMN_NAME_MOVIEID, rating.getMovieId());
-        values.put(RatingTable.RatingEntry.COLUMN_NAME_USERID, rating.getUserId());
-
-        return super.save(rating, "ratings", values);
-    }
-
-    /*
-    public void update(Rating rating, ContentValues values) {
-        super.update(rating, values);
-    }
-    */
-
-    /*
-
-    public void update(Rating rating, ContentValues values) {
-        super.update(rating, values, "ratings");
-    }
-
-    public long delete(Rating rating) {
-        super.delete(rating, "ratings");
-        return rating.getId();
-    }
-
-    /*
-    public List<Rating> getRatingFromUser(){
-
-    }
-
-    public List<Rating> getRatingFromMovie(){
-
-    }
-    */
-
 }
