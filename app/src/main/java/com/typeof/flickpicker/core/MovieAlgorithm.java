c474e0e072c6e61f0958a4e236f7037663a0a3b1
Jonathan Sundkvist <jonathansundkvist@hotmail.com>
2016-05-03 17:27:36 +0200

    Created the class MovieAlgorithm and its method getRecommendations

---
+++ app/src/main/java/com/typeof/flickpicker/core/MovieAlgorithm.java
@@ -0,0 +1,24 @@
+package com.typeof.flickpicker.core;
+
+import java.util.List;
+
+/**
+ * FlickPicker
+ * Group 22
+ * Created on 03/05/16.
+ */
+public class MovieAlgorithm {
+
+
+    /**
+     * Method for getting movies recommended for the user based on algorithm.
+     * @return List of movies recommended for the user.
+     */
+    public static List<Movie> getRecommendations(User user){
+
+
+
+        return null;
+    }
+
+}
