f15f0f6ec76b4caedb6da97c0e5883fb04c8378a
Jonathan Sundkvist <jonathansundkvist@hotmail.com>
2016-05-03 15:45:45 +0200

    created recommendationsActivity

---
+++ app/src/main/java/com/typeof/flickpicker/activities/recommendationsActivity.java
@@ -0,0 +1,18 @@
+package com.typeof.flickpicker.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.typeof.flickpicker.R;
+
+public class recommendationsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendations);
+
+    }
+}
