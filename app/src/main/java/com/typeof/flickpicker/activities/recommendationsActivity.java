bbaf6930f830f76752a0f54e977a9327c5e7b251
Jonathan Sundkvist <jonathansundkvist@hotmail.com>
2016-05-03 17:28:39 +0200

    created getRecommendations method in recommendationsActivity

--- app/src/main/java/com/typeof/flickpicker/activities/recommendationsActivity.java
+++ app/src/main/java/com/typeof/flickpicker/activities/recommendationsActivity.java
@@ -2,17 +2,44 @@ package com.typeof.flickpicker.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.typeof.flickpicker.R;
+import com.typeof.flickpicker.core.Movie;
+import com.typeof.flickpicker.core.MovieAlgorithm;
+import com.typeof.flickpicker.core.User;
 
+import java.util.List;
+
+
+/**
+ * FlickPicker
+ * Group 22
+ * Created on 03/05/16.
+ */
 public class recommendationsActivity extends AppCompatActivity {
 
-
+    private ListView mMovieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendations);
+
+
+
+    }
+
+    /**
+     * Method for getting movies recommended for the user.
+     * @param user
+     * @return recommendedMovies List of movies recommended for the user.
+     */
+    public List<Movie> getRecommendations(User user){
+
+        //create and return list of recommended movies based on algorithm
+        List<Movie> recommendedMovies = MovieAlgorithm.getRecommendations(user);
+        return recommendedMovies;
 
     }
 }
