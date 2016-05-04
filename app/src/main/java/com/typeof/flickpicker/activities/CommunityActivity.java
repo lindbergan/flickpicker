aa662b1c72ddeb54cf22b970a0f854f89530c7f9
Jolo87 <nylundj@student.chalmers.se>
2016-05-04 17:37:20 +0200

    work in progress...

--- app/src/main/java/com/typeof/flickpicker/activities/CommunityActivity.java
+++ app/src/main/java/com/typeof/flickpicker/activities/CommunityActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.roughike.bottombar.BottomBar;
 import com.typeof.flickpicker.R;
 import com.typeof.flickpicker.core.Movie;
 import com.typeof.flickpicker.database.MovieDAO;
@@ -25,14 +26,14 @@ import java.util.List;
  */
 public class CommunityActivity extends AppCompatActivity {
 
-    //Instance variables (mainly views)
+    //Instance variables
     private Button mTopMoviesButton;
     private Button mTopThisYearButton;
     private Button mWorstMoviesButton;
     private TextView mMovieText;
     private int desiredSizeOfList = 3;
     MovieDAO mSQLMovieDAO = new App().getMovieDAO();
-    SeedData mSeedData = new SeedData();
+    //SeedData mSeedData = new SeedData();
 
 
     @Override
@@ -40,28 +41,28 @@ public class CommunityActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.community_main);
 
-        ////////////////////////
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        //////////////////////////
+        //Feed the database with dummy Data
+        //mSeedData.seed();
 
         //Hook up views (Buttons, TextFields Cells etc...)
+        hookUpViews();
+
+        //Connect the listsners to the relevant views
+        setUpClickListeners();
+
+    }
+
+    public void hookUpViews(){
+
         mTopMoviesButton = (Button) findViewById(R.id.topPicksButton);
         mWorstMoviesButton = (Button) findViewById(R.id.worstMovieButton);
         mTopThisYearButton = (Button) findViewById(R.id.topThisYearButton);
         mMovieText = (TextView) findViewById(R.id.showMovieText);
 
-        //Feed the database with dummy Data
-        mSeedData.seed();
+    }
+
+    public void setUpClickListeners(){
+
 
         //setup listeners here
         mTopMoviesButton.setOnClickListener(new View.OnClickListener() {
@@ -71,28 +72,21 @@ public class CommunityActivity extends AppCompatActivity {
                 mMovieText.setText(topPicks.get(0).getTitle());
             }
         });
+        mWorstMoviesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Movie> worstPick = mSQLMovieDAO.getMostDislikedMovies(desiredSizeOfList);
+                mMovieText.setText(worstPick.get(0).getTitle());
+            }
+        });
+        mTopThisYearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Movie> topThisYear = mSQLMovieDAO.getTopRecommendedMoviesThisYear(desiredSizeOfList, 2016);
+                mMovieText.setText(topThisYear.get(0).getTitle());
+            }
+        });
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/typeof/flickpicker/activities/MainActivity.java
+++ app/src/main/java/com/typeof/flickpicker/activities/MainActivity.java
@@ -29,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        
+
     }
 
     @Override
