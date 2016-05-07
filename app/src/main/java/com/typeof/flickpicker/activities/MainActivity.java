9ce786e037b47ecb3137e4277edef907e8e6e3b6
Jonathan Sundkvist <jonathansundkvist@hotmail.com>
2016-05-07 16:32:22 +0200

    more fixing with TabHost, new method: configureTabs to setup the tabs

--- app/src/main/java/com/typeof/flickpicker/activities/MainActivity.java
+++ app/src/main/java/com/typeof/flickpicker/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.typeof.flickpicker.activities;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,10 +10,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.typeof.flickpicker.R;
 
+import java.lang.reflect.Type;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    TabHost tabHost;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +29,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        tabHost = (TabHost)findViewById(R.id.tabHost);
+        tabHost.setup();
+        configureTabs();
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+
+
+
+            }
+        });
+
     }
 
+    public void configureTabs() {
+
+
+        final TabHost.TabSpec mTabSpecRecommendations = tabHost.newTabSpec("Recommendations");
+        mTabSpecRecommendations.setContent(R.id.tabRecommendations);
+        mTabSpecRecommendations.setIndicator("Recommendations");
+        tabHost.addTab(mTabSpecRecommendations);
+
+        final TabHost.TabSpec mTabSpecCommunity = tabHost.newTabSpec("Community");
+        mTabSpecCommunity.setContent(R.id.tabCommunity);
+        mTabSpecCommunity.setIndicator("Community");
+        tabHost.addTab(mTabSpecCommunity);
+
+        final TabHost.TabSpec mTabSpecFriendsActivities = tabHost.newTabSpec("FriendsActivities");
+        mTabSpecFriendsActivities.setContent(R.id.tabFriendsActivities);
+        mTabSpecFriendsActivities.setIndicator("Friends' activities");
+        tabHost.addTab(mTabSpecFriendsActivities);
+
+        final TabHost.TabSpec mTabSpecMyMovies = tabHost.newTabSpec("MyMovies");
+        mTabSpecMyMovies.setContent(R.id.tabMyMovies);
+        mTabSpecMyMovies.setIndicator("My Movies");
+        tabHost.addTab(mTabSpecMyMovies);
+
+    }
+
+    //Unnecessary since we don't have a action bar???
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -28,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    //Unnecessary since we don't have a action bar???
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
