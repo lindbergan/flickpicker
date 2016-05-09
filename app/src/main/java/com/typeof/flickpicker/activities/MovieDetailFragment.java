f872054187b709889a7e32ed198811f065de9537
Jonathan Sundkvist <jonathansundkvist@hotmail.com>
2016-05-09 16:39:21 +0200

    added imageview and textview

--- app/src/main/java/com/typeof/flickpicker/activities/MovieDetailFragment.java
+++ app/src/main/java/com/typeof/flickpicker/activities/MovieDetailFragment.java
@@ -1,9 +1,39 @@
 package com.typeof.flickpicker.activities;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.typeof.flickpicker.R;
+
+
 /**
  * FlickPicker
  * Group 22
  * Created on 09/05/16.
  */
-public class MovieDetailFragment {
+public class MovieDetailFragment extends Fragment {
+
+    private ImageView movieImage;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Populate list with MovieCells
+        View movieDetailView = inflater.inflate(R.layout.activity_movie_detail, container, false);
+
+        return movieDetailView;
+    }
+
+
 }
