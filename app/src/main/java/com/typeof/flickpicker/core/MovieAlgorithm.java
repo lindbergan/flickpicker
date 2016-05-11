d835196a4eb971e5b371466dcf88130bbedf98fd
Jolo87 <nylundj@student.chalmers.se>
2016-05-11 11:56:47 +0200

    a first attempt at recommendationFragment

--- app/src/main/java/com/typeof/flickpicker/core/MovieAlgorithm.java
+++ app/src/main/java/com/typeof/flickpicker/core/MovieAlgorithm.java
@@ -1,5 +1,11 @@
 package com.typeof.flickpicker.core;
 
+import com.typeof.flickpicker.activities.App;
+import com.typeof.flickpicker.database.FriendDAO;
+import com.typeof.flickpicker.database.MovieDAO;
+import com.typeof.flickpicker.database.RatingDAO;
+import com.typeof.flickpicker.database.UserDAO;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,15 +16,36 @@ import java.util.List;
  */
 public class MovieAlgorithm {
 
-
     /**
      * Method for getting movies recommended for the user based on algorithm.
      * @return List of movies recommended for the user.
      */
     public static List<Movie> getRecommendations(User user){
 
-        List<Movie> results = new ArrayList<>();
         //algorithm
+        List<Movie> results = new ArrayList<>();
+
+        //fetch friends latest movie ratings;
+        List<Rating> friendsRecommendations = App.getFriendDAO().getFriendsLatestActivities(user.getId());
+
+        //convert them into movies:
+        List<Movie> friendsLatestMovies = new ArrayList<Movie>();
+
+        //loop through the ratings and extract the movies:
+        for (int i = 0; i<friendsLatestMovies.size(); i++){
+
+            long currentMovieId = friendsRecommendations.get(i).getMovieId();
+            Movie currentMovie = App.getMovieDAO().findMovie(currentMovieId);
+            friendsLatestMovies.add(currentMovie);
+        }
+
+        //...now do the magic:
+        for(int i = 0; i<friendsLatestMovies.size(); i++){
+
+            Movie currentMovie = friendsLatestMovies.get(i);
+            //currentMovie
+
+        }
 
         return results;
     }
