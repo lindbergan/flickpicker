2666d7d96285e8587601582ae36598069dbef7aa
Jonathan Sundkvist <jonathansundkvist@hotmail.com>
2016-05-12 15:45:56 +0200

    created RatingHelper class

---
+++ app/src/main/java/com/typeof/flickpicker/utils/RatingHelper.java
@@ -0,0 +1,23 @@
+package com.typeof.flickpicker.utils;
+
+import com.typeof.flickpicker.activities.App;
+import com.typeof.flickpicker.database.RatingDAO;
+
+/**
+ * FlickPicker
+ * Group 22
+ * Created on 12/05/16.
+ */
+public class RatingHelper {
+
+
+    public static void createNewRating(long userId, long movieId, double rating){
+
+        RatingDAO ratingDAO = App.getRatingDAO();
+
+
+    }
+
+
+
+}
