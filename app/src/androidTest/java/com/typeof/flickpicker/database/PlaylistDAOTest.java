6ff70ac2dd6b808fc0997ad82af496589c0849cc
Adrian Lindberg <adrlin@student.chalmers.se>
2016-05-13 18:07:33 +0200

    Changed playlists to playlist in test

--- app/src/androidTest/java/com/typeof/flickpicker/database/PlaylistDAOTest.java
+++ app/src/androidTest/java/com/typeof/flickpicker/database/PlaylistDAOTest.java
@@ -2,7 +2,9 @@ package com.typeof.flickpicker.database;
 import android.test.AndroidTestCase;
 
 import com.typeof.flickpicker.activities.App;
+import com.typeof.flickpicker.core.Movie;
 import com.typeof.flickpicker.core.Playlist;
+import com.typeof.flickpicker.core.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,14 +17,17 @@ import java.util.List;
 public class PlaylistDAOTest extends AndroidTestCase {
 
     private PlaylistDAO mPlaylistDAO;
+    private MovieDAO mMovieDAO;
     private Database mDatabase;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        mPlaylistDAO = App.getPlaylistDAO();
         mDatabase = App.getDatabase();
+        mDatabase.dropTables();
         mDatabase.setUpTables();
+        mPlaylistDAO = App.getPlaylistDAO();
+        mMovieDAO = App.getMovieDAO();
 
     }
 
@@ -32,17 +37,29 @@ public class PlaylistDAOTest extends AndroidTestCase {
         mDatabase.dropTables();
     }
 
-    public void testGetUserPlaylists() {
-        Playlist playlist = new Playlist("My favourites", 8, new ArrayList<Number>(){{add(20); add(30); add(40);}});
-        long id1 = mPlaylistDAO.savePlaylist(playlist);
-        Playlist playlist2 = new Playlist("My worst movies", 8, new ArrayList<Number>(){{add(20); add(30); add(40);}});
-        long id2 = mPlaylistDAO.savePlaylist(playlist2);
-        Playlist playlist3 = new Playlist("My coolest movies", 8, new ArrayList<Number>(){{add(20); add(30); add(40);}});
-        long id3 = mPlaylistDAO.savePlaylist(playlist3);
+    public void testGetUserPlaylist() {
 
-        List<Playlist> playlists = mPlaylistDAO.getUserPlaylists(8);
+        User currentUser = new User("CurrentUser", "admin");
+        long currentUserId = App.getUserDAO().saveUser(currentUser);
 
-        assertEquals(3, playlists.size());
+        Movie m1 = new Movie("Movie1", 1995);
+        Movie m2 = new Movie("Movie2", 1996);
+        Movie m3 = new Movie("Movie3", 1997);
+        mMovieDAO.saveMovie(m1);
+        mMovieDAO.saveMovie(m2);
+        mMovieDAO.saveMovie(m3);
+
+        List<Number> movieIds = new ArrayList<>();
+        movieIds.add(m1.getId());
+        movieIds.add(m2.getId());
+        movieIds.add(m3.getId());
+
+
+        Playlist playlist = new Playlist("Watchlist", currentUserId, movieIds);
+        mPlaylistDAO.savePlaylist(playlist);
+
+        Playlist resultPlaylist = mPlaylistDAO.getPlaylist(currentUserId);
+        assertTrue(resultPlaylist.getMovieIds().size() == 3);
     }
 
     public void testFindPlaylistById() {
