edc1744063de01e83dc814c50f7128fec67ca9bb
Jolo87 <nylundj@student.chalmers.se>
2016-05-20 21:48:59 +0200

    RatingHelper class finished(calls updateFriendMatches() && check if movie  previosly been rated by user. sets score accordingly )

--- app/src/main/java/com/typeof/flickpicker/utils/RatingHelper.java
+++ app/src/main/java/com/typeof/flickpicker/utils/RatingHelper.java
@@ -1,8 +1,12 @@
 package com.typeof.flickpicker.utils;
 
 import com.typeof.flickpicker.activities.App;
+import com.typeof.flickpicker.core.Movie;
 import com.typeof.flickpicker.core.Rating;
+import com.typeof.flickpicker.core.User;
 import com.typeof.flickpicker.database.RatingDAO;
+
+import java.util.List;
 
 /**
  * FlickPicker
@@ -11,25 +15,47 @@ import com.typeof.flickpicker.database.RatingDAO;
  */
 public class RatingHelper {
 
-
-
     /**
-     * static helper method for creating a new rating
+     * A static helper method which is called when a new rating is created.
+     * The method saves the newly created rating, update the dismatch value of the user
+     * and its friends accordingly and also sets the correct score.
      * @param rating
      * @param movieId
      * @param userId
      */
     public static void createNewRating(double rating, long movieId, long userId){
 
-        RatingDAO ratingDAO = App.getRatingDAO();
         Rating newRating = new Rating(rating, movieId, userId);
-        ratingDAO.saveRating(newRating);
+        App.getRatingDAO().saveRating(newRating);
+
+        //update the dismatch values in relation to users new rating
         App.getFriendDAO().updateFriendMatches(newRating);
 
-        //updateUserMatching();
+        if(!previousRatingExists(newRating)) {
+
+            //set the user's score accordingly
+            User currentUser = App.getCurrentUser();
+            int oldScore = currentUser.getScore();
+            int newScore = oldScore + 10;
+            currentUser.setScore(newScore);
+        }
     }
 
-    //private static void updateUserMatching(){}
+    /**
+     * A method the determines whether or not the user of the rating has rated the movie before.
+     * @param rating
+     * @return true if movie was previously rated by user. Otherwise false.
+     */
+    private static boolean previousRatingExists(Rating rating){
 
+        //extract the movie && userId from the raiting object and check if the movie already exists in the users movie collection
+        long ratingsUser = rating.getUserId();
+        Movie ratedMovie = App.getMovieDAO().findMovie(rating.getMovieId());
+        int desiredSizeOfList = 100;
 
+        List<Movie> usersMovieCollection = App.getMovieDAO().getMovieCollectionFromUserId(desiredSizeOfList, ratingsUser);
+
+        return usersMovieCollection.contains(ratedMovie);
+
+    }
 }
