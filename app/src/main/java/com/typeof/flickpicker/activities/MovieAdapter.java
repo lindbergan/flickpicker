07b6d63742ffe0a2768ec667a662e4b7083e272a
Jonathan Sundkvist <jonathansundkvist@hotmail.com>
2016-05-23 18:37:43 +0200

    cleanup in MovieAdapter and some fixing on layout

--- app/src/main/java/com/typeof/flickpicker/activities/MovieAdapter.java
+++ app/src/main/java/com/typeof/flickpicker/activities/MovieAdapter.java
@@ -1,7 +1,5 @@
 package com.typeof.flickpicker.activities;
 
-import android.app.Activity;
-import android.app.FragmentManager;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -9,85 +7,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.typeof.flickpicker.R;
 import com.typeof.flickpicker.core.Movie;
 
-import org.w3c.dom.Text;
+
+/**
+ * MovieAdapter extends CustomAdapter
+ * Used to define list items in lists that show movies objects
+ */
 
 public class MovieAdapter extends CustomAdapter {
 
-    private Context mContext;
+    Typeface font = Typeface.createFromAsset(getContext().getAssets(), "fonts/fontawesome-webfont.ttf");
 
     public MovieAdapter(Context context, Object[] obj) {
         super(context, obj);
-        mContext = context;
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        final Movie mov = (Movie) getItem(position);
-        ViewHolder viewHolder;
-
-        if (convertView == null) {
-            viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.custom_row_community, parent, false);
-            viewHolder.movieName = (TextView) convertView.findViewById(R.id.moviename_textview_movieCell);
-            viewHolder.movieGenre = (TextView) convertView.findViewById(R.id.rowCommunityGenre);
-            viewHolder.movieYear = (TextView) convertView.findViewById(R.id.rowCommunityYear);
-            viewHolder.friendsIcon = (TextView) convertView.findViewById(R.id.rowCommunityFriendsIcon);
-            viewHolder.friendsText = (TextView) convertView.findViewById(R.id.rowCommunityFriendsText);
-            viewHolder.communityIcon = (TextView) convertView.findViewById(R.id.rowCommunityCommunityIcon);
-            viewHolder.communityText = (TextView) convertView.findViewById(R.id.rowCommunityCommunityText);
-            viewHolder.moviePoster = (ImageView) convertView.findViewById(R.id.imageView_movieCell);
-
-            convertView.setTag(viewHolder);
-        }
-        else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-
-        convertView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MovieDetailFragment movieDetailFragment = new MovieDetailFragment();
-                Bundle args = new Bundle();
-                args.putLong("movieId", mov.getId());
-                movieDetailFragment.setArguments(args);
-                MainActivity mainActivity = (MainActivity) getContext();
-
-                KeyboardHelper.hideSoftKeyboard(mainActivity);
-
-                SingleFragmentHelper.setFragment(mainActivity, movieDetailFragment);
-            }
-        });
-
-        Typeface font = Typeface.createFromAsset(mContext.getAssets(), "fonts/fontawesome-webfont.ttf");
-
-        // Set text on movie name
-        viewHolder.movieName.setText(mov.getTitle());
-
-        viewHolder.movieGenre.setText(mov.getGenre());
-        viewHolder.movieYear.setText(String.valueOf(mov.getYear()));
-
-        viewHolder.friendsIcon.setTypeface(font);
-        int numOfFriendsSeen = App.getMovieDAO().getFriendsSeenMovie(mov.getId(), App.getCurrentUser().getId()).size();
-        viewHolder.friendsText.setText(String.valueOf(numOfFriendsSeen) + " friends have seen this");
-
-        viewHolder.communityIcon.setTypeface(font);
-        double communityRating = round(mov.getCommunityRating(), 1);
-        viewHolder.communityText.setText("rated " + String.valueOf(communityRating) + " by the users");
-
-        Picasso.with(getContext()).load(mov.getPoster()).into(viewHolder.moviePoster);
-        return convertView;
-    }
-
-
+    /**
+     * Private static class ViewHolder
+     * Used for caching values
+     */
     private static class ViewHolder {
 
         TextView movieName;
@@ -98,6 +41,65 @@ public class MovieAdapter extends CustomAdapter {
         TextView communityIcon;
         TextView communityText;
         ImageView moviePoster;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+        final Movie movie = (Movie) getItem(position);
+
+        if (view == null) {
+
+            viewHolder = new ViewHolder();
+
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            view = inflater.inflate(R.layout.custom_row_community, parent, false);
+
+            viewHolder.movieName = (TextView) view.findViewById(R.id.rowCommunityMovieTitle);
+            viewHolder.movieGenre = (TextView) view.findViewById(R.id.rowCommunityGenre);
+            viewHolder.movieYear = (TextView) view.findViewById(R.id.rowCommunityYear);
+            viewHolder.friendsIcon = (TextView) view.findViewById(R.id.rowCommunityFriendsIcon);
+            viewHolder.friendsText = (TextView) view.findViewById(R.id.rowCommunityFriendsText);
+            viewHolder.communityIcon = (TextView) view.findViewById(R.id.rowCommunityCommunityIcon);
+            viewHolder.communityText = (TextView) view.findViewById(R.id.rowCommunityCommunityText);
+            viewHolder.moviePoster = (ImageView) view.findViewById(R.id.rowCommunityImageView);
+            view.setTag(viewHolder);
+
+        }else{
+
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MovieDetailFragment movieDetailFragment = new MovieDetailFragment();
+                Bundle args = new Bundle();
+                args.putLong("movieId", movie.getId());
+                movieDetailFragment.setArguments(args);
+                MainActivity mainActivity = (MainActivity) getContext();
+
+                KeyboardHelper.hideSoftKeyboard(mainActivity);
+
+                SingleFragmentHelper.setFragment(mainActivity, movieDetailFragment);
+            }
+        });
+
+        viewHolder.movieName.setText(movie.getTitle());
+        viewHolder.movieGenre.setText(movie.getGenre());
+        viewHolder.movieYear.setText(String.valueOf(movie.getYear()));
+
+        viewHolder.friendsIcon.setTypeface(font);
+        int numOfFriendsSeen = App.getMovieDAO().getFriendsSeenMovie(movie.getId(), App.getCurrentUser().getId()).size();
+        viewHolder.friendsText.setText(String.valueOf(numOfFriendsSeen));
+
+        viewHolder.communityIcon.setTypeface(font);
+        double communityRating = round(movie.getCommunityRating(), 1);
+        viewHolder.communityText.setText(String.valueOf(communityRating));
+
+        Picasso.with(getContext()).load(movie.getPoster()).into(viewHolder.moviePoster);
+        return view;
     }
 
 
